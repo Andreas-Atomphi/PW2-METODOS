c26575cbe2dca0b8a04c7d3a06c817dabbf64eb0
Andreas-Atomphi <andreaseusoulin@gmail.com>
2020-03-04 20:01:57 -0300

    [FEAT] Revisão de if

---
+++ src/Exemplo.java
@@ -0,0 +1,21 @@
+import java.awt.Dialog;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+
+		int idade = 17;
+
+		if (idade >= 18) {
+
+			System.out.print("menor de idade");
+
+		} else {
+
+			System.out.print("maior de idade");
+
+		}
+		
+	}
+
+}
