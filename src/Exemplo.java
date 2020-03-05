e4c93469e8abb3a0d9c3c0cd566b0750019960af
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-03-04 22:01:04 -0300

    [FEAT] Exemplos de método com retorno

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -3,19 +3,63 @@ import java.awt.Dialog;
 public class Exemplo {
 
 	public static void main(String[] args) {
-
 		int idade = 17;
 
-		if (idade >= 18) {
+		ehMaiorIdade(524);
+		classificar(20);
+		System.out.println("Nome:" + concatenar("Andreas", "Faria"));
+		System.out.println("Quantidade:" + tamanhoNomeCompleto()));
+	}
 
-			System.out.print("menor de idade");
+	 //public=todos acessam; 
+	 //void=não retorna valor; 
+	 //após a última coluna de palavras-chave, aidcionar o nome da função e os argumentos;
+	
+	public static void ehMaiorIdade(int idade) {
+
+		if (idade < 18) {
+
+			System.out.print("menor de idade\n");
 
 		} else {
 
-			System.out.print("maior de idade");
+			System.out.print("maior de idade\n");
 
 		}
-		
+	}
+
+	public static void classificar(int num) {
+		String mensagem;
+		if (num > 0) {
+
+			mensagem = "Positivo";
+
+		} else if (num < 0) {
+
+			mensagem = "Negativo";
+
+		} else {
+
+			mensagem = "Zero";
+
+		}
+
+		System.out.print(String.format("%s é %s\n", num, mensagem));
+
+	}
+
+	public static String concatenar(String nome, String sobrenome) {
+
+		String nomecompleto = String.format("%s %s", nome, sobrenome);
+
+		return nomecompleto;
+
+	}
+
+	public static int tamanhoNomeCompleto() {
+		String nomecompleto = concatenar("Andreas", "Faria");
+
+		return nomecompleto.length();
 	}
 
 }
