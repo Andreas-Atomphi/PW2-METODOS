f2e36e23e4ddbea623042935c35ad924a8752653
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-03-04 22:28:02 -0300

    [FEAT] Erro de digitação corrigido

---
+++ src/Exercício.java
@@ -0,0 +1,22 @@
+
+public class Exercício {
+
+	public static void main(String[] args) {
+
+		System.out.println(String.format("%d %d %d\n", somar(5, 4), proximo(5), dobro(5)));
+
+	}
+
+	public static int somar(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static int proximo(int num) {
+		return num + 1;
+	}
+
+	public static int dobro(int num) {
+		return num + num;
+	}
+
+}
